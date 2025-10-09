be6e3a524b3c35e1c4c4a74df1b8cbd66229adcb
JelleSchrijvers-PXL <12504040@student.pxl.be>
2025-10-09 15:08:57 +0200

    project created

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cs_MergeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
