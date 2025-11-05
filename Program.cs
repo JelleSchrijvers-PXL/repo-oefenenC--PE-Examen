b1f4fe695f84270d80180c7fb8d6dfbab2248dab
JelleSchrijvers-PXL <12504040@student.pxl.be>
2025-11-05 11:28:42 +0100

    Finshed intaliatsion arrays + two void for total calculation and avg calcution

--- Program.cs
+++ Program.cs
@@ -2,16 +2,41 @@
 {
     internal class Program
     {
+        static double CalculationTotalPerDay(double[] arrkWhperdaydevices)
+        {
+            double TotalPerDay = 0;
+            double Input;
+            for(int i = 0;i < arrkWhperdaydevices.Length;i++)
+            {
+                Input = arrkWhperdaydevices[i];
+                TotalPerDay += Input;
+            }
+            return TotalPerDay;
+        }
+        static double Calculationavg(double[] arrkWhperdaydevices)
+        {
+            double avg = 0;
+            double Input;
+            for (int i = 0; i < arrkWhperdaydevices.Length; i++)
+            {
+                Input = arrkWhperdaydevices[i];
+                avg += Input;
+            }
+            avg /= arrkWhperdaydevices.Length;
+            return avg;
+        }
         static void Main(string[] args)
         {
             // Intalatie
             string nameclient;
             int clientdevices;
-            double[] arrkWhperdaydevices = {};
-            string[] arrDevicesName= {};
-
+            double DevicekWhPerDay;
+            double[] arrkWhperdaydevices ;
+            string[] arrDevicesName;
+            arrkWhperdaydevices = new double[] {};
+            arrDevicesName = new string[] {};
             //declaratie
-            Console.Write("Hello, customer!!! \n What is your name : ");
+            Console.Write("Hello, customer!!!\n What is your name : ");
             nameclient = Console.ReadLine();
             do
             {
@@ -26,9 +51,12 @@
                 do
                 {
                     Console.Write("How much kWh per day, does your device use: ");
-                }while(!Double.TryParse(Console.ReadLine(), out double DevicekWhPerDay));
-
+                }while(!Double.TryParse(Console.ReadLine(), out DevicekWhPerDay));
+                arrDevicesName[Clientdevices] = NameDevice;
+                arrkWhperdaydevices[Clientdevices] = DevicekWhPerDay;
             }
+            CalculationTotalPerDay(arrkWhperdaydevices);
+            Calculationavg(arrkWhperdaydevices);
             // voorkomen derket afsluiten
             Console.ReadKey();
         }
