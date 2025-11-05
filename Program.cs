3434d3a9d9d2f05913a77f76584495b6f457bff1
JelleSchrijvers-PXL <12504040@student.pxl.be>
2025-11-05 10:58:43 +0100

    Begining of oef maked asks name and how many devices and loop is ready to read everything in now. Only need top put input in the arrays.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Intalatie
+            string nameclient;
+            int clientdevices;
+            double[] arrkWhperdaydevices = {};
+            string[] arrDevicesName= {};
+
+            //declaratie
+            Console.Write("Hello, customer!!! \n What is your name :");
+            nameclient = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("How many devices, do you have :");
+            }while(!int.TryParse(Console.ReadLine(), out clientdevices));
+
+            for(int Clientdevices = 0; Clientdevices >= clientdevices;Clientdevices++)
+            {
+                string NameDevice;
+                Console.Write($"what is the name of your {Clientdevices+1} device:");
+                NameDevice = Console.ReadLine();
+                do
+                {
+                    Console.Write("How much kWh per day, does your device use:");
+                }while(Double.TryParse(Console.ReadLine(), out double DevicekWhPerDay));
+
+            }
+            // voorkomen derket afsluiten
+            Console.ReadKey();
         }
     }
 }
